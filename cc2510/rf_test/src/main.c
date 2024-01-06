bff8e578ba579257fe3cba33ec4561bfaa94d770
HomeGo <zhangzhen2618@gmail.com>
2024-01-07 02:38:06 +0800

    add W25X to rftest

--- cc2510/rf_test/src/main.c
+++ cc2510/rf_test/src/main.c
@@ -1,5 +1,23 @@
 #include "cc2510fx.h"
 #include "ioCCxx10_bitdef.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+#define BV(x) (1 << (x))
+
+#define NFC_FD P1_1
+#define NFCFLASH_PWR P1_0
+#define FLASH_CS P1_4
+#define FLASH_MOSI P1_6
+#define FLASH_MISO P1_7
+#define FLASH_SCK P1_5
+
+#define NFC_B_FD 1
+#define NFCFLASH_B_PWR 0
+#define FLASH_B_CS 4
+#define FLASH_B_MOSI 6
+#define FLASH_B_MISO 7
+#define FLASH_B_SCK 5
 
 #define LED_SW_SUM              P2_2
 #define LED_SW_WHITE            P2_1
@@ -12,20 +30,42 @@ SFRX(TEST1, 0XDF24);
 SFRX(TEST2, 0XDF23);
 
 void write_data(char *data, unsigned char size);
+void radio_init(void);
+void system_clock_init(void);
+void NfcFlash_IoInit(void);
+void Flash_get_id(uint8_t *data_ptr);
 
 int main(){
 
+    system_clock_init();
+    radio_init();
+    NfcFlash_IoInit();
+
+    NFCFLASH_PWR = 1;
+    
+    while(1){
+        uint8_t id[3];
+        Flash_get_id(id);
+        write_data(id, 2);
+        // while(!(RFTXRXIF));
+        
+        for(unsigned short i = 0; i < 150; i++){
+            unsigned short j = 0x0fff;
+            while(j--);
+        }
+    } 
+    
+}
+
+void system_clock_init(void){
     SLEEP &= ~SLEEP_OSC_PD;
     while( !(SLEEP & SLEEP_XOSC_S) );
     CLKCON = (CLKCON & ~(CLKCON_CLKSPD | CLKCON_OSC)) | CLKSPD_DIV_1;
     while (CLKCON & CLKCON_OSC);
 
     SLEEP |= SLEEP_OSC_PD;
-    P2DIR |= 3 << 1;
-    P1DIR = 0xff;
-    LED_SW_WHITE = 0;
-    LED_SW_SUM = 1;
-
+}
+void radio_init(void){
     /* RF settings SoC: CC2510 */
     // config from smart studio 7.0 expert mode last config
     // Data rate 500kBaud Mod:MSK RX_BW: 812KHZ
@@ -52,26 +92,7 @@ int main(){
     PA_TABLE0 = 0xFE; // pa power setting 
     IOCFG2    = 0x2E; // radio test signal configuration (p1_7) 
 
-
-
-    // PKTLEN = 0x03;
-    // SYNC1 = 0XAA;
-    // SYNC0 = 0XAA;
-    char helo[] = "Hello cc2510 radio!!!";
-
-    while(1){
-        write_data(helo, sizeof(helo));
-        // while(!(RFTXRXIF));
-        
-        LED_SW_WHITE = !LED_SW_WHITE;
-        for(unsigned short i = 0; i < 150; i++){
-            unsigned short j = 0x0fff;
-            while(j--);
-        }
-    } 
-    
 }
-
 void write_data(char *da, unsigned char size){
     // enter SIDLE
     RFST = RFST_SIDLE;
@@ -92,4 +113,73 @@ void write_data(char *da, unsigned char size){
         RFD = *(da + i);
     }
 
+}
+
+void NfcFlash_IoInit(void)
+{
+    P1DIR |= BV(FLASH_B_CS);
+    FLASH_CS = 1; // Flas CS
+    NFCFLASH_PWR = 0;
+    P1DIR |= BV(NFCFLASH_B_PWR) // NFCPWR output
+        | BV(FLASH_B_SCK) 
+        | BV(FLASH_B_MOSI) 
+        | BV(FLASH_B_CS); 
+    P1DIR &= ~(BV(FLASH_B_MISO));
+    PERCFG |= 0x02;       // USART1 alternative 2 location SPI
+    U1CSR = 0;             // SPI mode/master/clear flags
+    P1SEL |= BV(FLASH_B_SCK) | BV(FLASH_B_MOSI) | BV(FLASH_B_MISO);                        // MISO/MOSI/CLK peripheral functions // SS仅当从机使用时才配置为外设
+    U1BAUD = 0;            // baud M
+    U1GCR = U1GCR_ORDER | 17; // SCK-low idle, DATA-1st clock edge, MSB first
+
+    P1DIR &= ~BV(NFC_B_FD);
+}
+
+inline void Flash_Select(bool select)
+{
+    FLASH_CS = !select;
+}
+
+static uint8_t Flash_ExchangeData(uint8_t da)
+{
+    U1DBUF = da;
+    while (U1CSR & 0x01)
+    {
+        *(__xdata volatile uint8_t*)0xFE00 = U1CSR;
+    }
+    return U1DBUF;
+}
+
+void spi_write(uint8_t da){
+    U1CSR &= ~U1CSR_TX_BYTE;
+    U1DBUF = da;
+    while((U1CSR & U1CSR_TX_BYTE));
+}
+
+void spi_read(uint8_t *da){
+    /* U1CSR |= U1CSR_RE;
+    U1CSR &= ~U1CSR_RX_BYTE;
+    while(!(U1CSR & U1CSR_RX_BYTE));
+    *da = U1DBUF; */
+    U1DBUF = 0x00;
+     while (U1CSR & U1CSR_ACTIVE);
+    *da = U1DBUF;
+
+    // U1CSR &= ~U1CSR_RE;
+}
+
+void Flash_get_id(uint8_t *data_ptr){
+    Flash_Select(true);
+    // Flash_ExchangeData(0x90u);
+    // Flash_ExchangeData(0x00u); // 3 Byte dummy
+    // Flash_ExchangeData(0x00u);
+    // Flash_ExchangeData(0x00u);
+    spi_write(0x90u);
+    spi_write(0x00u);
+    spi_write(0x00u);
+    spi_write(0x00u);
+    // *data_ptr = Flash_ExchangeData(0xFFu); // 0xEF
+    // *(data_ptr + 1) = Flash_ExchangeData(0xFFu); // 0x11
+    spi_read(data_ptr);
+    spi_read(data_ptr + 1);
+    Flash_Select(false);
 }
